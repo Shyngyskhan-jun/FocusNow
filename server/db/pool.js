91de9267c65fc0fc6b096cc0bf262528414a8fa9
BinarySquad121 <amangeldinovtamirlan132@gmail.com>
2026-04-11 14:40:37 +0500

    поправка

--- server/db/pool.js
+++ server/db/pool.js
@@ -4,15 +4,20 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-const pool = new Pool({
-  user:     process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host:     process.env.DB_HOST,
-  port:     Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  ssl: process.env.NODE_ENV === 'production'
-  ? { rejectUnauthorized: false }
-  : false,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        user:     process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host:     process.env.DB_HOST,
+        port:     Number(process.env.DB_PORT),
+        database: process.env.DB_NAME,
+        ssl: false,
+      }
+);
 
 export default pool;
