dcec1a5585edf19303ca68657c80a8a1bbc10021
Shyngyskhan-jun <sygyszaksykeldi@gmail.com>
2026-06-25 20:12:49 +0500

    update Port

--- server/index.js
+++ server/index.js
@@ -61,5 +61,5 @@ app.use('/api/analytics', analyticsRoutes);
 // Обработка ошибок (должна быть последней)
 app.use(errorHandler);
 
-const PORT = process.env.PORT ?? 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
