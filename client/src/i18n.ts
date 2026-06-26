1be225e06e047d76028c33c26247eb68e8b913b7
Shyngyskhan-jun <sygyszaksykeldi@gmail.com>
2026-06-26 20:24:35 +0500

    localization through i18next fixed

--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -1,34 +1,31 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpBackend from 'i18next-http-backend'; // <-- Импортируем плагин загрузки
+
+// Импортируем файлы напрямую из твоей текущей папки src
+import translationRU from './locales/ru/translation.json';
+import translationEN from './locales/en/translation.json';
 
 i18n
-  // 1. Подключаем загрузчик JSON-файлов
-  .use(HttpBackend)
-  // 2. Подключаем автоматическое определение языка пользователя
-  .use(LanguageDetector)
-  // 3. Интегрируем i18n с React
   .use(initReactI18next)
-  // 4. Инициализируем настройки
   .init({
-    fallbackLng: 'ru', // Если язык не найден, включаем русский
-    debug: false,      // Поставь true, если захочешь видеть логи загрузки в консоли браузера
-    keySeparator: false,
-    nsSeparator: false,
-    backend: {
-      // Путь, откуда плагин будет забирать файлы перевода.
-      // {{lng}} автоматически заменится на 'ru' или 'en', а {{ns}} на 'translation'
-
-      // for local
-    //   loadPath: '/src/locales/{{lng}}/{{ns}}.json', 
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
-      // for vercel
+    resources: {
+      ru: {
+        translation: translationRU
+      },
+      en: {
+        translation: translationEN
+      }
     },
+    lng: localStorage.getItem('i18nextLng') || 'ru', // текущий язык
+    fallbackLng: 'ru',
+    
+    // Отключаем разделение по точкам, так как у нас плоский JSON
+    keySeparator: false, 
+    nsSeparator: false,
 
     interpolation: {
-      escapeValue: false, // React сам защищает от XSS атак
-    },
+      escapeValue: false, 
+    }
   });
 
 export default i18n;
